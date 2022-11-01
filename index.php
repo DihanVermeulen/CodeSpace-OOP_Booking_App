07b11a851e2bf9863f41bf1f7d3e7a305390821b
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-01 17:29:32 +0200

    Updated

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,9 +8,11 @@
     <link rel="stylesheet" href="src/styles/app.css">
     <title>Booking App</title>
 </head>
+
 <body>
-    <div class="app">
-        
+    <div id="app">
+
     </div>
 </body>
+
 </html>
